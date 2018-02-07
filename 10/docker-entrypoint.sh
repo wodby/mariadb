4d05c746fbc916d9d0267c311772857d86556d99
Chingis Sandanov <chingis@wodby.com>
2018-02-07 14:25:21 +0700

    Securely fix volumes permissions

--- 10/docker-entrypoint.sh
+++ 10/docker-entrypoint.sh
@@ -6,7 +6,7 @@ if [[ -n "${DEBUG}" ]]; then
     set -x
 fi
 
-sudo fix-permissions.sh mysql mysql /var/lib/mysql "${BACKUPS_DIR}"
+sudo fix-volumes-permissions.sh
 
 gotpl "/etc/gotpl/${MARIADB_VER:0:4}/my.cnf.tpl" > "/etc/mysql/my.cnf"
 

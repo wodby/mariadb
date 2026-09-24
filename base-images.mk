68ba732f0431b86ab70b0ca66e02d939dce19b20
Wodby Robot <robot@wodby.com>
2026-09-24 07:02:54 +0000

    Rebuild against updated base image digests

--- base-images.mk
+++ base-images.mk
@@ -3,7 +3,7 @@
 BASE_IMAGE_REPOSITORY := wodby/alpine
 BASE_IMAGE_VERSION_SUFFIX :=
 
-BASE_IMAGE_DIGEST_3.22 := sha256:7ac9658f4791afc64b2b1f27b80e26b03d86e4657ae4e1b348d000a9644a2295
+BASE_IMAGE_DIGEST_3.22 := sha256:fb9118dcd141e778eee08b7dcf5b12e14bccc2adc99fd315178ef1d7f0cedcf0
 BASE_IMAGE_DIGEST_3.22-r0 := sha256:7e36a0f87d5558c7a92e862ca68fd3c4c82341b638b8a500a82fe275c8a70667
 
 # Fail before building when a version or variant has no reviewed pin.
